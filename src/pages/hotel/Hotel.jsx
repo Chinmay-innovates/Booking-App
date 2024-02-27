7e39fc9ba0cb9ff55115fb107d997b5732696d07
Chinmay <chinmaygope69@gmail.com>
2024-02-27 20:38:09 +0530

    Add Booking App UI

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -51,65 +51,65 @@ const Hotel = () => {
       newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
     }
 
-    setSlideNumber(newSlideNumber)
+    setSlideNumber(newSlideNumber);
   };
 
   return (
     <div>
       <Navbar />
-      <Header type="list" />
-      <div className="hotelContainer">
+      <Header type='list' />
+      <div className='hotelContainer'>
         {open && (
-          <div className="slider">
+          <div className='slider'>
             <FontAwesomeIcon
               icon={faCircleXmark}
-              className="close"
+              className='close'
               onClick={() => setOpen(false)}
             />
             <FontAwesomeIcon
               icon={faCircleArrowLeft}
-              className="arrow"
+              className='arrow'
               onClick={() => handleMove("l")}
             />
-            <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+            <div className='sliderWrapper'>
+              <img src={photos[slideNumber].src} alt='' className='sliderImg' />
             </div>
             <FontAwesomeIcon
               icon={faCircleArrowRight}
-              className="arrow"
+              className='arrow'
               onClick={() => handleMove("r")}
             />
           </div>
         )}
-        <div className="hotelWrapper">
-          <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Tower Street Apartments</h1>
-          <div className="hotelAddress">
+        <div className='hotelWrapper'>
+          <button className='bookNow'>Reserve or Book Now!</button>
+          <h1 className='hotelTitle'>Tower Street Apartments</h1>
+          <div className='hotelAddress'>
             <FontAwesomeIcon icon={faLocationDot} />
             <span>Elton St 125 New york</span>
           </div>
-          <span className="hotelDistance">
+          <span className='hotelDistance'>
             Excellent location – 500m from center
           </span>
-          <span className="hotelPriceHighlight">
+          <span className='hotelPriceHighlight'>
             Book a stay over $114 at this property and get a free airport taxi
           </span>
-          <div className="hotelImages">
+          <div className='hotelImages'>
             {photos.map((photo, i) => (
-              <div className="hotelImgWrapper" key={i}>
+              <div className='hotelImgWrapper' key={i}>
                 <img
                   onClick={() => handleOpen(i)}
                   src={photo.src}
-                  alt=""
-                  className="hotelImg"
+                  alt=''
+                  className='hotelImg'
                 />
               </div>
             ))}
           </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle">Stay in the heart of City</h1>
-              <p className="hotelDesc">
+          <div className='hotelDetails'>
+            <div className='hotelDetailsTexts'>
+              <h1 className='hotelTitle'>Stay in the heart of City</h1>
+              <p className='hotelDesc'>
                 Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
                 Street Apartments has accommodations with air conditioning and
                 free WiFi. The units come with hardwood floors and feature a
@@ -118,12 +118,12 @@ const Hotel = () => {
                 also offered, as well as an electric tea pot and a coffee
                 machine. Popular points of interest near the apartment include
                 Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
+                airport is John Paul II International Kraków Balice, 16.1 km
                 from Tower Street Apartments, and the property offers a paid
                 airport shuttle service.
               </p>
             </div>
-            <div className="hotelDetailsPrice">
+            <div className='hotelDetailsPrice'>
               <h1>Perfect for a 9-night stay!</h1>
               <span>
                 Located in the real heart of Krakow, this property has an
